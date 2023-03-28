013e59f6fa41e60b688b86f7e73edc0ae6ebe530
shivv24 <127775144+shivv24@users.noreply.github.com>
2023-03-28 22:20:27 +0530

    Update index.js

--- http-server/index.js
+++ http-server/index.js
@@ -1,6 +1,7 @@
+
 const http = require("http");
 const fs = require("fs");
-const argv = require('minimist')(process.argv.slice(2));
+const argv = require("minimist")(process.argv.slice(2));
 
 let homeContent = "";
 let projectContent = "";
@@ -27,24 +28,27 @@ fs.readFile("registration.html", (err, registration) => {
   registrationContent = registration;
 });
 
-http.createServer((request, response) => {
-  let url = request.url;
-  response.writeHeader(200, { "Content-Type": "text/html" });
-  switch (url) {
-    case "/project":
-      response.write(projectContent);
-      response.end();
-      break;
-    case "/registration":
-      response.write(registrationContent);
-      response.end();
-      break;
-    default:
-      response.write(homeContent);
-      response.end();
-      break;
-  }
-});
-const port = argv.port || 3000;
+const port = argv.port || argv.p || 3000;
 
-server.listen(port);
+http
+  .createServer((request, response) => {
+    let url = request.url;
+    response.writeHeader(200, { "Content-Type": "text/html" });
+    switch (url) {
+      case "/project":
+        response.write(projectContent);
+        response.end();
+        break;
+      case "/registration":
+        response.write(registrationContent);
+        response.end();
+        break;
+      default:
+        response.write(homeContent);
+        response.end();
+        break;
+    }
+  })
+  .listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+  });
