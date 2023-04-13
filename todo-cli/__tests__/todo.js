7f2b66ff3cc6ea7e50c7552274103b11e9ab7153
shivv24 <127775144+shivv24@users.noreply.github.com>
2023-04-13 20:13:25 +0530

    Update todo.js

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,41 +1,41 @@
 const todoList = require("../todo");
 
 const {
-all,
-markAsComplete,
-add,
-overdue,
-dueToday,
-dueLater,
-toDisplayableList,
+  all,
+  markAsComplete,
+  add,
+  overdue,
+  dueToday,
+  dueLater,
+  toDisplayableList,
 } = todoList();
 
 describe("TodoList Test Suite", () => {
-beforeAll(() => {
-add({
-title: "new todo",
-completed: false,
-dueDate: new Date().toLocaleDateString("en-CA"),
-});
-});
+  beforeAll(() => {
+    add({
+      title: "new todo",
+      completed: false,
+      dueDate: new Date().toLocaleDateString("en-CA"),
+    });
+  });
 
-test("Should add new todo", () => {
-const todoItemCount = all.length;
-add({
-title: "Test todo",
-completed: false,
-dueDate: new Date().toLocaleDateString("en-CA"),
-});
-expect(all.length).toBe(todoItemCount + 1);
-});
+  test("Should add new todo", () => {
+    const todoItemCount = all.length;
+    add({
+      title: "Test todo",
+      completed: false,
+      dueDate: new Date().toLocaleDateString("en-CA"),
+    });
+    expect(all.length).toBe(todoItemCount + 1);
+  });
 
-test("Should mark a todo as complete", () => {
-expect(all[0].completed).toBe(false);
-markAsComplete(0);
-expect(all[0].completed).toBe(true);
-});
+  test("Should mark a todo as complete", () => {
+    expect(all[0].completed).toBe(false);
+    markAsComplete(0);
+    expect(all[0].completed).toBe(true);
+  });
 
-test("Should retrieve overdue items", () => {
+  test("Should retrieve overdue items", () => {
     const today = new Date().toISOString().split("T")[0];
     const overdueItems = all.filter(
       (item) =>
@@ -43,28 +43,27 @@ test("Should retrieve overdue items", () => {
         item.completed === false &&
         item.title !== "Submit assignment"
     );
-    expect(overdue().length).toEqual(overdueItems.length); // Remove +1 from expected value
+    expect(overdue().length).toEqual(overdueItems.length);
   });
-  
 
-describe("Due Today Items", () => {
-test("Should retrieve due today items", () => {
-const today = new Date().toISOString().slice(0, 10);
-const dueTodayItems = all.filter((item) => item.dueDate === today);
-expect(dueToday().length).toEqual(dueTodayItems.length);
-});
-});
+  describe("Due Today Items", () => {
+    test("Should retrieve due today items", () => {
+      const today = new Date().toISOString().slice(0, 10);
+      const dueTodayItems = all.filter((item) => item.dueDate === today);
+      expect(dueToday().length).toEqual(dueTodayItems.length);
+    });
+  });
 
-describe("Due Later Items", () => {
-test("Should retrieve due later items", () => {
-const today = new Date().toISOString().split("T")[0];
-const dueLaterItems = all.filter(
-(item) =>
-item.dueDate > today &&
-item.completed === false &&
-item.title !== "Submit assignment"
-);
-expect(dueLater().length).toEqual(dueLaterItems.length);
-});
-});
+  describe("Due Later Items", () => {
+    test("Should retrieve due later items", () => {
+      const today = new Date().toISOString().split("T")[0];
+      const dueLaterItems = all.filter(
+        (item) =>
+          item.dueDate > today &&
+          item.completed === false &&
+          item.title !== "Submit assignment"
+      );
+      expect(dueLater().length).toEqual(dueLaterItems.length);
+    });
+  });
 });
