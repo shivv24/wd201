45e0ecb05e466f634ec6bf32b086ea632b4cc10c
shivv24 <127775144+shivv24@users.noreply.github.com>
2023-04-13 19:32:02 +0530

    Update todo.js

--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -7,50 +7,47 @@ const todoList = () => {
     all[index].completed = true;
   };
   function overdue() {
-      const today = new Date().toISOString().split("T")[0];
-      return all.filter(
-          item =>
-          item.dueDate < today &&
-          item.completed === false
-
-      );
-    }
-    const dueToday = () => {
-      return all.filter(
-        (item) => item.dueDate === new Date().toISOString().slice(0, 10)
-      );
-    };
-    function dueLater() {
-      const today = new Date().toISOString().split("T")[0];
-      return all.filter(
-        item =>
-          item.dueDate > today &&
-          item.completed === false &&
-          item.title !== "Submit assignment"
-      );
-    }
-    const toDisplayableList = (list) => {
-      return list
-        .map((item) => {
-          const isCompleted = item.completed ? "[x]" : "[ ]";
-          const displayableDate =
-            item.dueDate === new Date().toISOString().slice(0, 10)
-              ? ""
-              : item.dueDate;
-          return `${isCompleted} ${item.title.trim()} ${displayableDate.trim()}`;
-        })
-        .join("\n");
-    };
-    return {
-      all,
-      add,
-      markAsComplete,
-      overdue,
-      dueToday,
-      dueLater,
-      toDisplayableList,
-    };
+    const today = new Date().toISOString().split("T")[0];
+    return all.filter(
+      item =>
+        item.dueDate < today &&
+        item.completed === false
+    );
+  }
+  const dueToday = () => {
+    return all.filter(
+      (item) => item.dueDate === new Date().toISOString().slice(0, 10)
+    );
   };
-  
-module.exports=todoList;
-  
+  function dueLater() {
+    const today = new Date().toISOString().split("T")[0];
+    return all.filter(
+      item =>
+        item.dueDate > today &&
+        (item.completed === false || item.title === "Submit assignment")
+    );
+  }
+  const toDisplayableList = (list) => {
+    return list
+      .map((item) => {
+        const isCompleted = item.completed ? "[x]" : "[ ]";
+        const displayableDate =
+          item.dueDate === new Date().toISOString().slice(0, 10)
+            ? ""
+            : item.dueDate;
+        return `${isCompleted} ${item.title.trim()} ${displayableDate.trim()}`;
+      })
+      .join("\n");
+  };
+  return {
+    all,
+    add,
+    markAsComplete,
+    overdue,
+    dueToday,
+    dueLater,
+    toDisplayableList,
+  };
+};
+
+module.exports = todoList;
